03bbe55a547a39f50dd6da7632a4aabe91046247
Colleague Riley <ColleagueRiley@gmail.com>
2023-10-08 16:44:56 -0400

    some minor changes

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #define SILICON_IMPLEMENTATION
 #include "silicon.h"
 
-int main() {
-    si_initNS();
-    
+int main() {    
 	NSApplication NSApp = NSApplication_sharedApplication();
 	
     NSApplication_setActivationPolicy(NSApp, NSApplicationActivationPolicyRegular);
-    
-    NSWindow win = NSWindow_init(NSRect_init(500, 500, 200, 200), NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable, NSBackingStoreBuffered, false);
-    
+
+    NSWindow win = NSWindow_init(NSMakeRect(500, 500, 200, 200), 
+                                    NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable, NSBackingStoreBuffered, 
+                                    false
+                                );
+
     NSWindow_makeKeyAndOrderFront(win, nil);
 
 	NSApplication_run(NSApp);
